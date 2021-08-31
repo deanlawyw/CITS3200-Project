cabf31dfd782203c1610c32594422c1c4a8bcf58
Yaxin <dyjyx1234@163.com>
2021-08-31 11:41:51 +0800

    add firefox webdriver

--- test/integrationTest.py
+++ test/integrationTest.py
@@ -2,56 +2,72 @@ import os
 import unittest
 import pandas as pd
 from selenium import webdriver
+import selenium
 from selenium.webdriver.common.keys import Keys
+import subprocess
 
 
 class Test(unittest.TestCase):
-
-    # declare variable to store the url to be visited
-    base_url = ""
-    # declare variable to store search term
-    search_term = ""
 
     def setUp(self) -> None:
 
         path = os.getcwd()
         # declare and initialize driver
-        self.driver = webdriver.Chrome(os.path.join(path, "chromedriver"))
-
-        # browser loads in maximized window
-        self.driver.maximize_window()
-        # driver waits implicitly for 10 seconds, for the element under consideration to load
-        self.driver.implicitly_wait(10)
+        while True:
+            driver_choice = input(
+                "please select the webdriver, chrome or firefox: \n")
+            if driver_choice.lower() == "chrome":
+                self.driver = webdriver.Chrome(
+                    os.path.join(path, "chromedriver"))
+                break
+            elif driver_choice.lower() == "firefox":
+                self.driver = webdriver.Firefox(
+                    os.path.join(path, "geckodriver"))
+                break
+        subprocess.check_call("npm start", shell=True)
 
     def testSample(self) -> None:
 
+        pass
+        # # browser loads in maximized window
+        # self.driver.maximize_window()
+        # # driver waits implicitly for 5 seconds, for the element under consideration to load
+        # self.driver.implicitly_wait(5)
+
         # load a given url in browser window
-        self.driver.get(self.base_url)
+        # self.driver.get(self.base_url)
 
-        # fetech the content with xpath
-        content_list = self.driver.find_elements_by_xpath(
-            "//h3/span[contains(text(),'')]/../..//table//td")
-        item_list = []
+        # # fetech the content with xpath
+        # content_list = self.driver.find_elements_by_xpath(
+        #     "//h3/span[contains(text(),'')]/../..//table//td")
+        # item_list = []
 
-        # collect the result in a dataframe
-        df = pd.DataFrame(item_list)
+        # # collect the result in a dataframe
+        # df = pd.DataFrame(item_list)
 
-        # find search box
-        search_box = self.driver.find_element_by_id("")
-        # enter the search term in the search textbox
-        search_box.send_keys(self.search_term)
+        # # find search box
+        # search_box = self.driver.find_element_by_id("")
+        # # enter the search term in the search textbox
+        # search_box.send_keys(self.search_term)
 
-        # to search for the entered search term
-        search_box.send_keys(Keys.RETURN)
-        # to click on the first search result's link
-        self.driver.find_element_by_xpath("").click()
-        # switch to the new tab
-        self.driver.switch_to.window(self.driver.window_handles[1])
-        # to confirm if a certain page has loaded
-        self.assertTrue(self.driver.title.startswith(""))
-        # to confirm if a certain item is visible or not
-        self.assertTrue(self.driver.find_element_by_id("").is_displayed())
+        # # to search for the entered search term
+        # search_box.send_keys(Keys.RETURN)
+        # # to click on the first search result's link
+        # self.driver.find_element_by_xpath(
+        #     "//a[@href='/admin']").click()
+        # # switch to the new tab
+        # self.driver.switch_to.window(self.driver.window_handles[1])
+        # # to confirm if a certain page has loaded
+        # self.assertTrue(self.driver.title.startswith(""))
+        # # to confirm if a certain item is visible or not
+        # self.assertTrue(self.driver.find_element_by_id("").is_displayed())
 
     def shutDown(self) -> None:
 
         self.driver.quit()
+
+
+if __name__ == "__main__":
+    test = Test()
+    test.setUp()
+    test.testSample()
